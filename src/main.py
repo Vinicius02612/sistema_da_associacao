2890821264dde599c85bf353949f9b0cacf4e5f3
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-11-22 09:37:28 -0300

    criando rotas para users

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI, HTTPException,status
 from routers import router_user_associate
 from connection.database import Base, engine
 from models.models import User
-Base.metadata.drop_all(bind=engine)
+
+
+#Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
