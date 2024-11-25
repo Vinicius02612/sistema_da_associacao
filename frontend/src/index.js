4999cc5ac4b02f8437f31d836608d7e7191f2c98
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-11-25 10:59:29 -0300

    criando rotas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import Login from './routes/Login/Login';
+import Cadastro from './routes/Cadastro/Cadastro';
+
+const BrowserRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/cadastro',
+    element: <Cadastro />,
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={BrowserRouter}/>
   </React.StrictMode>
 );
 
