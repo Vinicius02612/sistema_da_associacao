26c8e2d77511e57ababb13f7a7b9da3e1a7de3fd
Aleff Ericlys <ericlys558@gmail.com>
2025-05-14 14:43:58 -0300

    feat: tela para adicionar sócio

---
+++ Frontend/src/views/AddSocio.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="socios">
+		<v-card class="d-flex align-center justify-center">
+			<v-card-text>
+				<v-img src="@/assets/Logo1.png" max-width="400px"></v-img>
+				<h1>Welcome to the Socios Page</h1>
+			</v-card-text>
+		</v-card>
+	</div>
+</template>
