7ca0d9f734eadb1f7440ff3a17b0271e7fdd9c63
Aleff Ericlys <ericlys558@gmail.com>
2025-05-29 05:29:27 -0300

    feat: card para a tabela de projetos em andamento

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -8,11 +8,11 @@
 						<v-btn
 							v-bind="props"
 							class="ma-4 text-h6"
-							@click=""
 							prepend-icon="mdi-account-plus"
 							:color="isHovering ? 'primary' : ''"
 							size="x-large"
 							stacked
+							to="/socios/adicionar"
 						>
 							Adicionar sócio
 						</v-btn>
@@ -21,7 +21,6 @@
 						<v-btn
 							v-bind="props"
 							class="ma-4 text-h6"
-							@click=""
 							prepend-icon="mdi-calendar-month"
 							:color="isHovering ? 'primary' : ''"
 							size="x-large"
@@ -34,11 +33,11 @@
 						<v-btn
 							v-bind="props"
 							class="ma-4 text-h6"
-							@click=""
 							prepend-icon="mdi-plus-circle-outline"
 							:color="isHovering ? 'primary' : ''"
 							size="x-large"
 							stacked
+							to="/projetos/adicionar"
 						>
 							Novo Projeto
 						</v-btn>
@@ -47,93 +46,33 @@
 			</v-col>
 
 			<v-col cols="12">
-				<v-table>
-					<thead>
-						<tr>
-							<th>Titulo</th>
-							<th>CNPJ</th>
-							<th>Instituição/Empresa</th>
-							<th>Data de Inicio</th>
-							<th>Data de Término</th>
-							<th>Status</th>
-							<th>Ações</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr
-							v-for="(item, index) in numbers"
-							:key="item.id"
-							:style="{ backgroundColor: index % 2 === 0 ? 'white' : '#f8f8f8' }"
-						>
-							<td>{{ item.id }}</td>
-							<td>
-								<PhoneLabel v-if="item.number" :data="item.number" />
-								<template v-else>Não conectado</template>
-							</td>
-							<td>{{ item.name }}</td>
-							<td><DateLabel :date="new Date(item.fstConnection)" /> </td>
-							<td><DateLabel :date="new Date(item.lastConnection)" /> </td>
-							<td>
-								<v-chip
-									:color="getStatusColor(item.status)"
-									:append-icon="getStatusIcon(item.status)"
-									class="chip-size d-flex justify-space-between"
-									size="large"
-									variant="tonal"
-								>
-									{{ getStatusText(item.status) }}
-								</v-chip>
-							</td>
-							<td>
-								<v-btn-group
-									variant="outlined"
-									divided
-									rounded="lg"
-									class="custom-btn-group d-flex align-center"
-								>
-									<v-btn
-										width="32"
-										height="32"
-										v-tooltip:top="'Visualizar'"
-										@click="viewNumber(item)"
-									>
-										<v-icon size="x-large">mdi-pencil</v-icon>
-									</v-btn>
-									<v-btn
-										width="32"
-										height="32"
-										v-tooltip:top="dialogAction(item.status).label"
-										@click="
-											openDialogAction(dialogAction(item.status).action, item.id)
-										"
-									>
-										<v-icon size="x-large">
-											mdi-trash-can-outline
-										</v-icon>
-									</v-btn>
-								</v-btn-group>
-							</td>
-						</tr>
-					</tbody>
-				</v-table>
+				<v-card>
+					<v-card-title>
+						Projetos em andamento
+					</v-card-title>
+					<v-card-text>
+
+					</v-card-text>
+				</v-card>
 			</v-col>
-			
 		</v-row>
 	</v-container>	
 </template>
 
 <script>
+
 export default {
 	name: 'Home',
 	data() {
 		return {
-			message: 'Hello, Vue!'
+			message: 'Hello, Vue!',
 		};
 	},
+	
 	methods: {
 		greet() {
 			alert(this.message);
-		}
+		},
 	},
 };
 </script>
