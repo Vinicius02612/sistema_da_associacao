c0c8f4afd3e47aa7b7adbbec0f98add8264cdae8
Aleff Ericlys <ericlys558@gmail.com>
2025-05-29 05:30:42 -0300

    feat: tabela funcional

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -52,6 +52,74 @@
 					</v-card-title>
 					<v-card-text>
 
+						<v-table>
+							<thead>
+								<tr>
+									<th>Titulo</th>
+									<th>CNPJ</th>
+									<th>Instituição/Empresa</th>
+									<th>Data de Inicio</th>
+									<th>Data de Término</th>
+									<th>Status</th>
+									<th>Ações</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr
+									v-for="(item, index) in projetosFiltered"
+									:key="item.id"
+									:style="{ backgroundColor: index % 2 === 0 ? 'white' : '#f8f8f8' }"
+								>
+									<td>{{ item.Titulo }}</td>
+									<td>{{ item.CNPJ }}</td>
+									<td>{{ item.InstituicaoEmpresa }}</td>
+									<td><DateLabel :date="new Date(item.dataInicio)" /> </td>
+									<td><DateLabel :date="new Date(item.dataFim)" /> </td>
+									<td>
+										<v-chip
+										:color="item.status === 'Finalizado' ? 'gray' : item.status === 'Cancelado' ? 'red' : 'green'"
+										class="chip-size d-flex justify-center"
+										variant="outlined"
+										>
+										{{ item.status }}
+									</v-chip>
+									</td>
+									<td>
+										<v-btn-group
+											variant="outlined"
+											divided
+											rounded="lg"
+											class="custom-btn-group d-flex align-center"
+										>
+											<v-btn
+												width="32"
+												height="32"
+											>
+												<v-tooltip
+													activator="parent"
+													location="bottom"
+												>
+													Editar
+												</v-tooltip>
+												<v-icon size="x-large">mdi-pencil</v-icon>
+											</v-btn>
+											<v-btn
+												width="32"
+												height="32"
+											>
+												<v-tooltip
+													activator="parent"
+													location="bottom"
+												>
+													Apagar
+												</v-tooltip>
+												<v-icon size="x-large">mdi-trash-can-outline</v-icon>
+											</v-btn>
+										</v-btn-group>
+									</td>
+								</tr>
+							</tbody>
+						</v-table>
 					</v-card-text>
 				</v-card>
 			</v-col>
@@ -60,6 +128,7 @@
 </template>
 
 <script>
+import DateLabel from '@/components/ui/DateLabel.vue';
 
 export default {
 	name: 'projetos',
