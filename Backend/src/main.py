d65a35ebf3623626d25d57916e1c7c435b98e96b
Vincius Nunes <viniciusnunes02612@gmail.com>
2025-06-25 22:51:34 -0300

    feat: criando cors da api

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from .routers import router_login, router_user_associate, router_projetos, router_despesas, router_receitas, router_mensalidade
 from connection.database import Base, engine
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 #Base.metadata.drop_all(bind=engine)
@@ -10,6 +10,13 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_login.router)
 app.include_router(router_user_associate.router)
